baa9b381bb735b0994294d38bf632fedafac4edf
Quentin Davillé <quentin.daville@epitech.eu>
2021-10-25 13:34:36 +0200

    [Add] display info user in setting

--- lib/post_model.dart
+++ lib/post_model.dart
@@ -1,17 +1,27 @@
 import "dart:convert";
+import 'dart:developer';
+
+void printWrapped(String text) {
+  final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
+  pattern.allMatches(text).forEach((match) => print(match.group(0)));
+}
 
 class UserData {
-  final String display_name;
+  final String name;
   final String icon_img;
   final String description;
+  final int total_karma;
 
-  UserData(this.display_name, this.icon_img, this.description);
+  UserData(this.name, this.icon_img, this.description, this.total_karma);
 }
 
 class Unserializer {
   UserData getUserDatafromJson(String str) {
-    final j = json.decode(str);
-    return UserData(j["userId"] as String, j["icon_img"] as String,
-        j["description"] as String);
+    Map j = jsonDecode(str);
+    return UserData(
+        j['name'] as String,
+        j["icon_img"] as String,
+        j['subreddit']["public_description"] as String,
+        j['total_karma'] as int);
   }
 }
